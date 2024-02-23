b82372980bd3496855c1b2b2fa852c1235b9c5d5
AdityaPunjcreator <adityajaiswal554@gmail.com>
2024-02-23 23:05:01 +0530

    baseURL change

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 // here we are storing our base url which will remain same in all our api
 
-const API_BASE_URL = "http://localhost:8000/api/v1/ecom";
+const API_BASE_URL = "https://e-commerce-backend-vve4.onrender.com/api/v1/ecom";
 export default API_BASE_URL;
